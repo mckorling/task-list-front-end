011914c68a2e41e3ed258eb0ff2e409fdaad4fc4
Megan Korling <korlingm@gmail.com>
2022-06-16 19:22:05 -0700

    modified css for toggling complete task

--- src/components/Task.js
+++ src/components/Task.js
@@ -5,14 +5,13 @@ import './Task.css';
 
 const Task = ({ id, title, isComplete }) => {
   const [complete, setComplete] = useState(isComplete);
-  const buttonClass = complete ? 'tasks__item__toggle--completed' : '';
+  const buttonClass = complete
+    ? 'tasks__item__toggle--completed'
+    : 'tasks__item__toggle';
 
   return (
     <li className="tasks__item">
-      <button
-        className={`tasks__item__toggle ${buttonClass}`}
-        onClick={() => setComplete(!complete)}
-      >
+      <button className={buttonClass} onClick={() => setComplete(!complete)}>
         {title}
       </button>
       <button className="tasks__item__remove button">x</button>
